40f675c67fad65879a98acb592dd0f25cf014ce8
Michael Stewart <stewarmc@jmu.edu>
2020-07-19 15:02:19 -0400

    responsive right drawer

--- src/index.js
+++ src/index.js
@@ -1,22 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-// import App from './components/App'
 import Root from './components/Root'
 import store from './app/store'
-// import { Provider } from 'react-redux'
 import * as serviceWorker from './serviceWorker'
-// import {createStore} from 'redux'
-
-// const store = createStore()
 
 ReactDOM.render(
   <Root store={store}/>,
-  // <React.StrictMode>
-  //   <Provider store={store}>
-  //     <App />
-  //   </Provider>
-  // </React.StrictMode>,
   document.getElementById('root')
 )
 
